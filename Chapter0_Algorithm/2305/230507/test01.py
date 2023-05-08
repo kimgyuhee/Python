892f5256987c336ee2f7fcf6ebec80b4394778a2
개발하는 김곰돌ᕕʕ •ₒ• ʔ୨ <rbgmll10331@gmail.com>
2023-05-08 13:40:21 +0900

    coding test
    level0

--- Chapter0_Algorithm/2305/230507/test01.py
+++ Chapter0_Algorithm/2305/230507/test01.py
@@ -10,6 +10,9 @@
 return할 배열이 빈 배열이라면 ["EMPTY"]를 return 합니다.
 """
 
+import re
+
+
 def solution(myStr):
     answer = []
     myStr = myStr.replace("a", " ")
@@ -33,3 +36,57 @@ def solution(myStr):
     return answer if answer else ['EMPTY']
 
 print(solution("baconlettucetomato"))
+
+"""
+문자열 myString과 pat가 주어집니다. 
+myString의 부분 문자열중 pat로 끝나는 가장 긴 부분 문자열을 찾아서 return 하는 solution 함수를 완성해 주세요.
+"""
+
+def solution(myString, pat):
+    idx = myString[::-1].find(pat[::-1])
+    print(idx)
+    if idx == 0 :
+        return myString
+    else :
+        return myString[:-idx]
+
+# 다른 사람 풀이 1
+solution=lambda x,y:x[:x.rindex(y)+len(y)]
+
+# 다른 사람 풀이 2
+def solution(myString, pat):
+    return myString[:len(myString) - myString[::-1].index(pat[::-1])]
+
+print(solution("AbCdaadEFG", "dE"))
+print(solution("AAAAaaaa", "a"))
+
+"""
+다음과 같이 출력하도록 코드를 작성해 주세요.
+
+>>> 출력예시
+>>> !@#$%^&*(\'"<>?:;
+"""
+
+print('hello world!')
+print("!@#$%^&*(\\\'\"<>?:;")
+
+# 공백 기준 분리
+text = "사과 딸기 수박 메론 바나나"
+t = re.split(" ", text)
+print(text)
+print(t)
+
+"""
+문자열 myString과 pat이 주어집니다.
+myString에서 pat이 등장하는 횟수를 return 하는 solution 함수를 완성해 주세요.
+"""
+
+def solution(myString, pat):
+    answer = 0
+    for i in range(len(myString)-len(pat)+1) :
+        print(myString[i:i+len(pat)+1])
+        if pat == myString[i:i+len(pat)] :
+            answer +=1
+    return answer
+
+print(solution("banana", "ana"))
--- Chapter0_Algorithm/2305/230507/test03.py
+++ Chapter0_Algorithm/2305/230507/test03.py
@@ -4,10 +4,19 @@
 flag를 차례대로 순회하며 flag[i]가 true라면 X의 뒤에 arr[i]를 arr[i] × 2 번 추가하고, 
 flag[i]가 false라면 X에서 마지막 arr[i]개의 원소를 제거한 뒤 X를 return 하는 solution 함수를 작성해 주세요.
 
-
 """
 
 def solution(arr, flag):
     answer = []
+    for a, f in zip(arr, flag) :
+        if f :
+            answer+=[a]*2*a
+        else :
+            answer = answer[:-a]
     return answer
+
+ad = 2
+print([ad]+[])
+d =[0,1,2,3,4,5,6,7,8]
+print(d[:-2])
 
