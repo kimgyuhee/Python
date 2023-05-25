ca6f0baaf1040dd9495926e06cbba0551fc2333f
개발하는 김곰돌ᕕʕ •ₒ• ʔ୨ <rbgmll10331@gmail.com>
2023-05-25 23:54:56 +0900

    coding test

--- Chapter0_Algorithm/2305/230525/test01.py
+++ Chapter0_Algorithm/2305/230525/test01.py
@@ -3,14 +3,33 @@
 
 query를 순회하면서 다음 작업을 반복합니다.
 
-짝수 인덱스에서는 arr에서 query[i]번 인덱스를 제외하고 배열의 query[i]번 인덱스 뒷부분을 잘라서 버립니다.
-홀수 인덱스에서는 arr에서 query[i]번 인덱스는 제외하고 배열의 query[i]번 인덱스 앞부분을 잘라서 버립니다.
+짝수 인덱스에서는 arr에서 query[i]번 인덱스를 제외하고 
+배열의 query[i]번 인덱스 뒷부분을 잘라서 버립니다.
+홀수 인덱스에서는 arr에서 query[i]번 인덱스는 제외하고 
+배열의 query[i]번 인덱스 앞부분을 잘라서 버립니다.
 위 작업을 마친 후 남은 arr의 부분 배열을 return 하는 solution 함수를 완성해 주세요.
 
 """
 
 def solution(arr, query):
     answer = []
-    return answer
+    for i in range(len(query)) :
+        value = query[i]
+        if i%2 == 0 :
+            arr = arr[:value+1]
+        else :
+            arr = arr[value:]
+    return arr
 
-print(solution([0, 1, 2, 3, 4, 5], [4, 1, 2], [1, 2, 3]))
+
+# 다른 사람 풀이
+def solution(arr, query):
+    for k, q in enumerate(query):
+        if k % 2 == 0:
+            arr = arr[:q + 1]
+        else:
+            arr = arr[q:]
+    return arr
+
+
+print(solution([0, 1, 2, 3, 4, 5], [4, 1, 2]))
