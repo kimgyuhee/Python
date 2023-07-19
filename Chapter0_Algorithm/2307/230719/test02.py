34df88b1ee0d410d32b1bd4dba469305dca9b102
개발하는 김곰돌ᕕʕ •ₒ• ʔ୨ <rbgmll10331@gmail.com>
2023-07-19 23:50:58 +0900

    coding test

--- Chapter0_Algorithm/2307/230719/test02.py
+++ Chapter0_Algorithm/2307/230719/test02.py
@@ -45,4 +45,21 @@ def solution(word):
     return dictionary.index(word) + 1
 
 
+# 다른 사람 풀이
+def solution(word):
+    answer = 0
+    alpha  = ["A","E","I","O","U",""]
+    ans = []
+    for i in alpha:
+        for j in alpha:
+            for k in alpha:
+                for l in alpha:
+                    for m in alpha:
+                        w = i+j+k+l+m
+                        if w not in ans: ans.append(w)
+    ans.sort()
+    answer = ans.index(word)
+    return answer
+
+
 print(solution("AAAAE"))
